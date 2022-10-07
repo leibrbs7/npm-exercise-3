b68df94de25bf26c6d89daa5129ac09bd9dda836
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-07 11:53:57 +0800

    whitespaces edited

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { parse, toString, toArray} = require('@estilles/expression-parser');
 const process = require('process');
+
 let args = process.argv.slice(2);
 let finalString = args.join(' ');
+
 console.log(toString(parse(finalString)));
 console.log(toArray(parse(finalString)));
